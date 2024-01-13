8eac8ad559537f0607e9538bc450e7817831ec2f
Kristian Nedrevold-Hansen <kristian@krined.no>
2024-01-13 15:03:16 +0100

    Updated jdtls to actually be active

--- config/luanix/nvim-jdtls.lua.nix
+++ config/luanix/nvim-jdtls.lua.nix
@@ -1,8 +1,10 @@
+# Setup for Java language server
 { pkgs, ... }:
 ''
---local config = {
---    cmd = {${pkgs.jdt-language-server}/bin/jdt-language-server},
---    root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
---}
---require('jdtls').start_or_attach(config)
+  local config = {
+      cmd = {${pkgs.jdt-language-server}/bin/jdt-language-server},
+      root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw', 'pom.xml'}, { upward = true })[1]),
+      on_attach = Lsp_on_attach
+  }
+  require('jdtls').start_or_attach(config)
 ''
