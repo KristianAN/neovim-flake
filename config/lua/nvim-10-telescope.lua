3714afb5af662da9b183109a4e8f23611de0664b
Kristian Nedrevold-Hansen <kristian@krined.no>
2024-01-13 15:35:27 +0100

    fixup jdtls

--- config/lua/nvim-10-telescope.lua
+++ config/lua/nvim-10-telescope.lua
@@ -1,25 +1,25 @@
 local opt = { noremap = true }
 require('telescope').setup {
-	defaults = {
-		mappings = {
-			i = {
-				['<C-u>'] = false,
-				['<C-d>'] = false,
-				["<C-j>"] = require('telescope.actions').move_selection_next,
-				["<C-k>"] = require('telescope.actions').move_selection_previous,
-			},
-		},
-	},
+  defaults = {
+    mappings = {
+      i = {
+        ['<C-u>'] = false,
+        ['<C-d>'] = false,
+        ["<C-j>"] = require('telescope.actions').move_selection_next,
+        ["<C-k>"] = require('telescope.actions').move_selection_previous,
+      },
+    },
+  },
 }
 
 vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
 vim.keymap.set('n', '<leader>fb', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
 vim.keymap.set('n', '<leader>/', function()
-	-- You can pass additional configuration to telescope to change theme, layout, etc.
-	require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-		winblend = 10,
-		previewer = false,
-	})
+  -- You can pass additional configuration to telescope to change theme, layout, etc.
+  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+    winblend = 10,
+    previewer = false,
+  })
 end, { desc = '[/] Fuzzily search in current buffer]' })
 
 vim.keymap.set('n', '<leader><space>', require('telescope.builtin').find_files, { desc = 'Search Files' })
--- config/luanix/nvim-jdtls.lua.nix
+++ config/luanix/nvim-jdtls.lua.nix
@@ -1,10 +1,27 @@
 # Setup for Java language server
 { pkgs, ... }:
 ''
+  local home = os.getenv('HOME')
+  local root_markers = { 'gradlew', 'mvnw', '.git' }
+  local root_dir = require('jdtls.setup').find_root(root_markers)
+  local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+  local workspace_dir = home .. "/.local/share/eclipse/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
+  local nvim_jdtls_group = vim.api.nvim_create_augroup("nvim-jdtls", { clear = true })
   local config = {
-      cmd = { "${pkgs.jdt-language-server}/bin/jdt-language-server" },
-      root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw', 'pom.xml'}, { upward = true })[1]),
-      on_attach = Lsp_on_attach
+    cmd = { '${pkgs.jdt-language-server}/bin/jdt-language-server', '-data', workspace_dir },
+    on_attach = Lsp_on_attach,
+    root_dir = root_dir,
   }
-  require('jdtls').start_or_attach(config)
+
+  local nvim_jdtls_group = vim.api.nvim_create_augroup("nvim-jdtls", { clear = true })
+  vim.api.nvim_create_autocmd(
+    "FileType",
+    {
+      pattern = { "java" },
+      callback = function()
+        require('jdtls').start_or_attach(config)
+      end,
+      group = nvim_jdtls_group,
+    }
+  )
 ''
