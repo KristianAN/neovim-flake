c2891fc52b0d4384e5724c01dbd93c0af91452d4
Kristian Nedrevold-Hansen <kristian@krined.no>
2024-01-13 15:07:14 +0100

    Updated jdtls to have correct cmd

--- config/luanix/nvim-jdtls.lua.nix
+++ config/luanix/nvim-jdtls.lua.nix
@@ -2,7 +2,7 @@
 { pkgs, ... }:
 ''
   local config = {
-      cmd = {${pkgs.jdt-language-server}/bin/jdt-language-server},
+      cmd = { "${pkgs.jdt-language-server}/bin/jdt-language-server" },
       root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw', 'pom.xml'}, { upward = true })[1]),
       on_attach = Lsp_on_attach
   }
