5f3d353a86cff55574a493159cf174af9e4bcb89
Kristian Nedrevold-Hansen <kristian@krined.no>
2024-04-25 22:07:25 +0200

    hmm

--- config/luanix/nvim-30-lspconfig.lua.nix
+++ config/luanix/nvim-30-lspconfig.lua.nix
@@ -69,7 +69,8 @@ in
     -- NOTE: You may or may not want java included here. You will need it if you
     -- want basic Java support but it may also conflict if you are using
     -- something like nvim-jdtls which also works on a java filetype autocmd.
-    pattern = { "scala", "sbt" },
+    pattern = { "scala", "sbt", "bleep.yaml" },
+
     callback = function()
       require("metals").initialize_or_attach(metals_config)
       require("metals").setup_dap()
