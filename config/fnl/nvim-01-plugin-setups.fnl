f43bcb51d973c9f0b349e74e36a6921882cf49bf
Kristian Nedrevold-Hansen <kristian@krined.no>
2024-09-10 01:21:56 +0200

    cleanup

---
+++ config/fnl/nvim-01-plugin-setups.fnl
@@ -0,0 +1,7 @@
+;; One-liner setups of plugins goes here
+(let [devicons (require :nvim-web-devicons)]
+  (devicons.setup))
+
+(let [ibl (require :ibl)]
+  (ibl.setup))
+
---
+++ config/lua/nvim-01-plugin-setups.lua
@@ -0,0 +1,7 @@
+-- [nfnl] Compiled from nvim-01-plugin-setups.fnl by https://github.com/Olical/nfnl, do not edit.
+do
+  local devicons = require("nvim-web-devicons")
+  devicons.setup()
+end
+local ibl = require("ibl")
+return ibl.setup()
