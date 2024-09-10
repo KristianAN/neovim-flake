aea3aee73f53f972fcd0df4474b0abc851a0cc55
Kristian Nedrevold-Hansen <kristian@krined.no>
2024-09-10 09:12:49 +0200

    migrate nvim-10-mini-hipatterns to fennel

---
+++ config/fnl/nvim-10-mini-hipatterns.fnl
@@ -0,0 +1,11 @@
+(let [hipatterns (require :mini.hipatterns)]
+  (hipatterns.setup {:highlighters {:fixme {:pattern "%f[%w]()FIXME()%f[%W]"
+                                            :group :MiniHipatternsFixme}
+                                    :hack {:pattern "%f[%w]()HACK()%f[%W]"
+                                           :group :MiniHipatternsHack}
+                                    :todo {:pattern "%f[%w]()TODO()%f[%W]"
+                                           :group :MiniHipatternsTodo}
+                                    :note {:pattern "%f[%w]()NOTE()%f[%W]"
+                                           :group :MiniHipatternsNote}}
+                     :hex_color (hipatterns.gen_highlighter.hex_color)}))
+
--- config/lua/nvim-10-mini-hipatterns.lua
+++ config/lua/nvim-10-mini-hipatterns.lua
@@ -1,13 +1,3 @@
-local hipatterns = require('mini.hipatterns')
-hipatterns.setup({
-  highlighters = {
-    -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
-    fixme = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
-    hack  = { pattern = '%f[%w]()HACK()%f[%W]',  group = 'MiniHipatternsHack'  },
-    todo  = { pattern = '%f[%w]()TODO()%f[%W]',  group = 'MiniHipatternsTodo'  },
-    note  = { pattern = '%f[%w]()NOTE()%f[%W]',  group = 'MiniHipatternsNote'  },
-
-    -- Highlight hex color strings (`#rrggbb`) using that color
-    hex_color = hipatterns.gen_highlighter.hex_color(),
-  },
-})
+-- [nfnl] Compiled from nvim-10-mini-hipatterns.fnl by https://github.com/Olical/nfnl, do not edit.
+local hipatterns = require("mini.hipatterns")
+return hipatterns.setup({highlighters = {fixme = {pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme"}, hack = {pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack"}, todo = {pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo"}, note = {pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote"}}, hex_color = hipatterns.gen_highlighter.hex_color()})
