30b8c22942a2a2a26e1aaf07c6967ed0a8366092
Kristian Nedrevold-Hansen <kristian@krined.no>
2024-09-10 08:24:50 +0200

    nvim-10-treesitter fennel

--- config/lua/nvim-10-treesitter.lua
+++ config/lua/nvim-10-treesitter.lua
@@ -1,10 +1,4 @@
-require 'nvim-treesitter.configs'.setup {
-  ensure_installed = {},
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
-  indent = {
-    enable = true
-  }
-}
+-- [nfnl] Compiled from nvim-10-treesitter.fnl by https://github.com/Olical/nfnl, do not edit.
+local treesitter = require("nvim-treesitter")
+do local _ = treesitter.configs.setup end
+return {ensure_installed = {}, highlight = {enable = true, additional_vim_regex_highlighting = false}, indent = {enable = true}}
