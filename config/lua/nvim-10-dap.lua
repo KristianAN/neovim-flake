97abeae715a3283038edb0f4ce6ebdd20bbfaad9
Kristian Nedrevold-Hansen <kristian@krined.no>
2024-09-11 10:37:19 +0200

    dap

--- config/lua/nvim-10-dap.lua
+++ config/lua/nvim-10-dap.lua
@@ -1,7 +1,3 @@
 -- [nfnl] Compiled from nvim-10-dap.fnl by https://github.com/Olical/nfnl, do not edit.
-do
-  local dap = require("dap")
-  dap.configurations.scala({[{metals = {runType = "runOrTestFile"}, name = "RunOrTest", request = "launch", type = "scala"}] = {type = "scala", request = "launch", metals = {runType = "testTarget"}}})
-end
 local dapui = require("dapui")
 return dapui.setup()
