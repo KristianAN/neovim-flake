76ba0ae0da9273e4d6c1720991dab4699de545f1
Kristian Nedrevold-Hansen <kristian@krined.no>
2024-09-24 15:28:08 +0200

    update flake

--- config/lua/nvim-01-mini.lua
+++ config/lua/nvim-01-mini.lua
@@ -4,22 +4,22 @@ do
   vim.cmd("au ColorScheme * highlight MiniNotifyNormal guibg=NONE")
   vim.cmd("au ColorScheme * highlight MiniNotifyTitle guibg=NONE")
   vim.cmd("au ColorScheme * highlight MiniNotifyBorder guibg=NONE")
-  mini_notify.setup({ lsp_progress = { enable = false }, window = { config = {}, winblend = 0 } })
+  mini_notify.setup({lsp_progress = {enable = false}, window = {config = {}, winblend = 0}})
 end
 do
   local mini_files = require("mini.files")
   vim.cmd("au ColorScheme * highlight MiniFilesNormal guibg=NONE")
   vim.cmd("au ColorScheme * highlight MiniFilesTitle guibg=NONE")
   vim.cmd("au ColorScheme * highlight MiniFilesBorder guibg=NONE")
-  mini_files.setup({ window = { config = {}, winblend = 0 } })
+  mini_files.setup({window = {config = {}, winblend = 0}})
   local function _1_()
     return mini_files.open()
   end
-  vim.keymap.set("n", "<leader>or", _1_, { desc = "open file browser" })
+  vim.keymap.set("n", "<leader>or", _1_, {desc = "open file browser"})
   local function _2_()
     return mini_files.open(vim.api.nvim_buf_get_name(0))
   end
-  vim.keymap.set("n", "<leader>oc", _2_, { desc = "open file browser in current dir" })
+  vim.keymap.set("n", "<leader>oc", _2_, {desc = "open file browser in current dir"})
 end
 do
   local mini_misc = require("mini.misc")
