0d0c45fcceb9cb652a22fb0584cb6df939573729
Kristian Nedrevold-Hansen <kristian@krined.no>
2024-09-25 00:06:17 +0200

    remove java from metals

--- config/luanix/nvim-30-lspconfig.lua.nix
+++ config/luanix/nvim-30-lspconfig.lua.nix
@@ -59,7 +59,7 @@ in
       -- NOTE: You may or may not want java included here. You will need it if you
       -- want basic Java support but it may also conflict if you are using
       -- something like nvim-jdtls which also works on a java filetype autocmd.
-      pattern = { "scala", "sbt", "bleep.yaml", "java" },
+      pattern = { "scala", "sbt", "bleep.yaml" },
 
       callback = function()
         require("metals").initialize_or_attach(metals_config)
