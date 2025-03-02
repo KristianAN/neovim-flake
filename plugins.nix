175f86f175869b876037a088208fd04a6f0ccfd8
Kristian Nedrevold-Hansen <kristian@krined.no>
2025-03-02 22:07:47 +0100

    Get treesitter for fsharp from nixpkgs

--- plugins.nix
+++ plugins.nix
@@ -1,52 +1,30 @@
 { pkgs }:
 let
-  treesitter-fsharp-grammar = pkgs.tree-sitter.buildGrammar {
-    language = "fsharp";
-    version = "0.0.0+rev=f54ac4e";
-
-    src = pkgs.fetchFromGitHub {
-      owner = "ionide";
-      repo = "tree-sitter-fsharp";
-      rev = "f54ac4e66843b5af4887b586888e01086646b515";
-      hash = "sha256-zKfMfue20B8sbS1tQKZAlokRV7efMsxBk7ySQmzLo0Y=";
-    };
-
-    fixupPhase = ''
-      mkdir -p $out/queries/fsharp
-      mv $out/queries/*.scm $out/queries/fsharp/
-    '';
-
-    meta.homepage = "https://github.com/ionide/tree-sitter-fsharp";
-  };
-
   nvim-treesitter = (
-    pkgs.vimPlugins.nvim-treesitter.withPlugins (
-      plugins:
-      [
-        plugins.scala
-        plugins.nix
-        plugins.java
-        plugins.haskell
-        plugins.python
-        plugins.rust
-        plugins.javascript
-        plugins.typescript
-        plugins.lua
-        plugins.yaml
-        plugins.xml
-        plugins.sql
-        plugins.smithy
-        plugins.dockerfile
-        plugins.css
-        plugins.kotlin
-        plugins.c_sharp
-        plugins.gleam
-        plugins.clojure
-        plugins.fennel
-        plugins.org
-      ]
-      ++ [ treesitter-fsharp-grammar ]
-    )
+    pkgs.vimPlugins.nvim-treesitter.withPlugins (plugins: [
+      plugins.fsharp
+      plugins.scala
+      plugins.nix
+      plugins.java
+      plugins.haskell
+      plugins.python
+      plugins.rust
+      plugins.javascript
+      plugins.typescript
+      plugins.lua
+      plugins.yaml
+      plugins.xml
+      plugins.sql
+      plugins.smithy
+      plugins.dockerfile
+      plugins.css
+      plugins.kotlin
+      plugins.c_sharp
+      plugins.gleam
+      plugins.clojure
+      plugins.fennel
+      plugins.org
+    ])
   );
 
 in
